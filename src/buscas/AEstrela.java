3fc1d16fdf22a44e1fa462c29bfd2267ee7a0c96
Gabriela Gimenez <81652916+GaybsGimenez@users.noreply.github.com>
2024-03-27 08:29:22 -0300

    add algoritmo de busca A*

---
+++ src/buscas/AEstrela.java
@@ -0,0 +1,46 @@
+package buscas;
+
+import estruturas.VetorOrdenadoAdjacente;
+import grafocidades.Adjacente;
+import grafocidades.Cidade;
+import grafocidades.Mapa;
+
+public class AEstrela {
+    private VetorOrdenadoAdjacente fronteira;
+    private Cidade objetivo;
+    private boolean achou;
+
+    public AEstrela(Cidade objetivo){
+        this.objetivo = objetivo;
+        achou = false;
+    }
+    public void buscar(Cidade atual) {
+        System.out.println("\n * Atual: " + atual.getNome());
+        atual.setVisitado(true);
+
+        if (atual == objetivo) {
+            achou = true;
+        }
+        else {
+            fronteira = new VetorOrdenadoAdjacente(atual.getAdjacentes().size());
+            for (Adjacente a: atual.getAdjacentes()) {
+                if(!a.getCidade().isVisitado()){
+                    a.getCidade().setVisitado(true);
+                    fronteira.inserir(a); // pararemtro é o adjacente
+                }
+            }
+
+            fronteira.mostrar();
+            buscar(fronteira.getPrimeiro());
+        }
+    }
+
+    // teste metodo main
+
+    public static void main(String args[]){
+        Mapa mapa = new Mapa();
+        AEstrela a = new AEstrela(mapa.getCuritiba()); // estado objetivo
+        a.buscar(mapa.getPortoUniao()); // estado inicial
+    }
+
+}
